51430affa06241851108875b834dd790af8a085b
Tathata <86846618+TathataHY@users.noreply.github.com>
2024-10-01 21:06:33 -0300

    portfolio project

---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,49 @@
+import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+export const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+  shouldSpin,
+  spinDuration,
+  shouldOrbit,
+  orbitDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+  shouldOrbit?: boolean;
+  orbitDuration?: string;
+}>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
+      <div
+        className={twMerge(shouldOrbit && "animate-spin")}
+        style={{ animationDuration: orbitDuration }}
+      >
+        <div
+          className="flex items-start justify-start"
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            width: `${size}px`,
+            height: `${size}px`,
+          }}
+        >
+          <div
+            className={twMerge(shouldSpin && "animate-spin")}
+            style={{ animationDuration: spinDuration }}
+          >
+            <div
+              className="inline-flex"
+              style={{ transform: `rotate(${rotation * -1}deg)` }}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
